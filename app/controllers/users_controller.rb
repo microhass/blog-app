09871d7ba49f543da1aec1a828608ff787444834
hassanShakur <hass.shakurhassan@gmail.com>
2023-09-27 15:48:18 +0300

    add user routes & controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get 'users/:user_id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:id', to: 'posts#show'
+
+  # New & edit
+  get '/users/new', to: 'users#new'
+  get '/users/:id/edit', to: 'users#edit'
+  get '/users/:user_id/posts/new', to: 'posts#new'
+  get '/users/:user_id/posts/:id/edit', to: 'posts#edit'
 end
