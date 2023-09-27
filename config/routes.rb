a5444c23d611378a77d05e36f568c05097b05093
hassanShakur <hass.shakurhassan@gmail.com>
2023-09-27 20:10:52 +0300

    create edit snd new user forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:edit, :update], path_names: {edit: 'edit'}
+
   # Defines the root path route ("/")
   root "users#index"
   get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get 'users/:user_id/posts', to: 'posts#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
-
-  # New & edit
   get '/users/new', to: 'users#new'
+  get '/users/:id', to: 'users#show'
   get '/users/:id/edit', to: 'users#edit'
+  get 'users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/new', to: 'posts#new'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
   get '/users/:user_id/posts/:id/edit', to: 'posts#edit'
+
 end
