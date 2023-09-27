3dc9cca488ea955e68679317b8b07d7dd09ae7da
hassanShakur <hass.shakurhassan@gmail.com>
2023-09-27 15:49:29 +0300

    add post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,29 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = params[:user_id]
+    @post.save
+
+    redirect_to user_posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to user_posts_path
+  end
 end
