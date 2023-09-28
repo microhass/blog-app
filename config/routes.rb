b3e2f7033bcdfea859dc23a3d54b9407f55d2672
hassanShakur <hass.shakurhassan@gmail.com>
2023-09-28 20:56:35 +0300

    add comment for first user

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:edit, :update, :new, :create], path_names: {edit: 'edit'}
-  resources :posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 
-  # Defines the root path route ("/")
+  resources :comments, only: [:new, :create, :destroy]
+
   root "users#index"
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  get '/users/:id', to: 'users#show'
-  get '/users/:id/edit', to: 'users#edit'
-  get 'users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/new', to: 'posts#new'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
-  get '/users/:user_id/posts/:id/edit', to: 'posts#edit'
-
-
 end
