7e52134f880d2fae16901cbfdd752e10b55c124e
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-01 19:43:50 +0300

    implemented .include in queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
-    puts params
   end
 
   def show
